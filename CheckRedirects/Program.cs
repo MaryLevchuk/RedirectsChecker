c143b0beb5e8d830708123bb31c5c6dff33e7ab8
MaryLevchuk <marylevchuk@gmail.com>
2017-07-09 22:37:52 +0300

    Initial commit

---
+++ CheckRedirects/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CheckRedirects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = ParseFilename(args);
+            var culture = ParseCulture(args);
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+
+            var results = new CheckResults();
+            IEnumerable<RedirectUrls> redirectList = ParseExcelFile(filename);
+          
+            foreach (var redirectUrls in redirectList)
+            {
+                var request = new Request { RequestUrl = redirectUrls.Old };
+                var client = new Client();
+                var response = client.Get(request);
+                var checkResult = Analyze(response, redirectUrls);
+                results.Add(checkResult);
+            }
+            Output(results);
+        }
+
+        private static string ParseCulture(string[] args)
+        {
+            var culture = args.Length > 1 ? args[1] : null;
+            culture = culture ?? ConfigurationManager.AppSettings["Culture"] ?? "en-US";
+            return culture;
+        }
+
+        private static void Output(CheckResults results)
+        {
+            foreach (var result in results.Items.Where(x => x.IsOk==false))
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        private static CheckResult Analyze(Response response, RedirectUrls urls)
+        {
+            var result = new CheckResult();
+
+            if (response.StatusCode == 301)
+            {
+                var redirectResponse = (RedirectResponse)response;
+                result.IsOk = (redirectResponse.RedirectUrl.Equals(urls.New, StringComparison.CurrentCultureIgnoreCase));
+                result.Details = $"{response.StatusCode} - {redirectResponse.RedirectUrl}";
+                
+            }
+            else
+            {
+                result.IsOk = false;
+                result.Details = urls.Old;
+            }
+            return result;
+            //var redirectResponse = response as RedirectResponse;
+
+            //if (redirectResponse != null)
+            //{ }
+            //else { }
+        }
+
+        private static IEnumerable<RedirectUrls> ParseExcelFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string ParseFilename(string[] args)
+        {
+            var filename = args.FirstOrDefault() ?? ConfigurationManager.AppSettings["SourceFile"] ?? "redirects.xlsx";
+            return filename;
+        }
+    }
+}
---
+++ CheckRedirects/RedirectUrls.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckRedirects
+{
+    public class RedirectUrls
+    {
+        public string Old { get; set; }
+        public string New { get; set; }
+    }
+
+
+
+    // --------
+
+    public class Response
+    {
+        public int StatusCode { get; set; }
+    }
+
+    public class RedirectResponse : Response
+    {
+        public string RedirectUrl { get; set; }
+    }
+
+    public class Request
+    {
+        public string RequestUrl { get; internal set; }
+    }
+
+    public class Client
+    {
+        public Response Get(Request request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CheckResult
+    {
+        public override string ToString()
+        {
+            if (IsOk)
+                return "OK";
+            else
+                return $"{Details}" ;
+        }
+        public bool IsOk { get; set; }
+        public string Details { get; set; }
+    }
+
+    public class CheckResults
+    {
+        public List<CheckResult> Items { get; } = new List<CheckResult>();
+        public void Add(CheckResult checkResult)
+        {
+            Items.Add(checkResult);
+            
+        }
+    }
+}
